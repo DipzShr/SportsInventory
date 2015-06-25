eb9c0f98a9e5b408c18747d2961eeee5fb9a8e3e
DipzShr <Dipz.shr@gmail.com>
2015-06-25 12:28:02 +0545

    Initialize git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsInventory::Application.config.secret_token = '0e53a1a7611801c0785a76393c8c3de20d04fcbcd597a43aa500ef753397eb2f606d84ce832e71e7d6f79a8e77523509176341d6d5684140e94f4a13485616eb'
